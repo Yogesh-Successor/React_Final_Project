0f1f7f72c5ee032426e323ff7927305b2f9269b5
Yogesh-Successor <56887847+Yogesh-Successor@users.noreply.github.com>
2023-10-03 16:47:24 +0530

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+//Components -- Function and Class
+
+function Main(){
+    return <>
+        <h2>Main Component is being rendered</h2>
+        <h4>React was going good</h4>
+            </>//React Fragment
+}//Functional component
+
+export default Main
+
+
+/* 
+default export
+-- One file one export accepted
+-- Import with any name
+
+named export
+-- 'n' number of datas can be exported
+-- Import same name
+ */
+/* 
+function display(){
+    //code
+}
+display()
+
+
+class Show{
+ display(){
+        //code
+    }
+}
+let show = new Show()
+show.display() */
